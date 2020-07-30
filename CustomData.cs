b990a30731c6b363750a47ef46fc843090b50d07
francoiscoetzee85 <58804557+francoiscoetzee85@users.noreply.github.com>
2020-07-30 16:47:46 -0700

    Create CustomData.cs

---
+++ CustomData.cs
@@ -0,0 +1,16 @@
+namespace CSharpCode
+       {
+           /// <summary>
+           /// Some custom data
+           /// </summary>
+           public class CustomData
+           {
+               private string _customDataInt;
+       
+               public string CustomDataInt
+               {
+                   get => _customDataInt;
+                   set => _customDataInt = value;
+               }
+           }
+       }
